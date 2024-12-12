3e8f0342df280dc52b495dc672ff3b4c7ef7e042
Juliya <iuliia.sharipova@gmail.com>
2024-12-12 15:56:19 +0100

    MapGenerator.cpp added
    class to generate the map

---
+++ robots_planning/src/MapGenerator.cpp
@@ -0,0 +1,73 @@
+#include "stdio.h"
+#include <vector>
+#include <map>
+#include <iostream>
+#include <iostream>
+
+struct Point
+{
+    int x;
+    int y;
+};
+
+struct Graph
+{
+    std::map<Point, std::vector<Point>> adjVector;
+};
+
+class MapGenerator
+{
+    public:
+        Graph G;
+
+    private:
+        Graph toGenerateMap(int interactions)
+        {
+            int count = 0;
+
+            Point init{0, 0};
+
+            while (count < interactions)
+            {
+                count ++;
+
+                Point newP{getRandPosition(-10, 10), getRandPosition(-10, 10)};
+
+                if (isItInObstacle(newP))
+                {
+                    continue;
+                }
+
+                Point nearestP = toFindNearest(G, newP);
+                G.adjVector.at(nearestP).push_back(newP);
+
+                if (isReachedGate(newP))
+                {
+                    return G;
+                }
+            }
+            return G;
+        };
+
+        float getRandPosition(float min, float max)
+        {
+            float random = min + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (max - min)));
+            return random;
+        };
+
+        // think if service or message from the Obstacle node
+        bool isItInObstacle(Point p)
+        {
+            return false;
+        };
+
+        Point toFindNearest(Graph g, Point p)
+        {
+            return (Point{0, 0});
+        };
+
+        bool isReachedGate(Point p)
+        {
+            return true;
+        };
+}
