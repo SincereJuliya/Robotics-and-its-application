ef7180f52a56dd307deb1bf4c9610749b16f62e1
Juliya <iuliia.sharipova@gmail.com>
2025-01-08 11:38:28 +0100

    Update IMapGenerator.cpp

--- robot_planning/src/IMapGenerator.cpp
+++ robot_planning/src/IMapGenerator.cpp
@@ -2,21 +2,8 @@
 #include <vector>
 #include <map>
 #include <iostream>
-#include <variables.hpp>
-
-
-/* 
-struct Point
-{
-    int x;
-    int y;
-};
-
-struct Graph
-{
-    std::map<Point, std::vector<Point>> adjVector;
-}; 
-*/
+#include <graph.hpp>
+#include <obstacles.hpp>
 
 /*
     1. the communication between nodes: services, messages...
@@ -29,31 +16,7 @@ class IMapGenerator
         Graph G;
 
         virtual Graph toGenerateMap(int interactions)
-        {
-            
-            /* int count = 0;
-
-            Point init{0, 0};
-
-            while (count < interactions)
-            {
-                count ++;
-
-                Point newP{getRandPosition(-10, 10), getRandPosition(-10, 10)};
-
-                if (isItInObstacle(newP))
-                {
-                    continue;
-                }
-
-                Point nearestP = toFindNearest(G, newP);
-                G.adjVector.at(nearestP).push_back(newP);
-
-                if (isReachedGate(newP))
-                {
-                    return G;
-                }
-            }*/
+        {           
             return G; 
         };
         
@@ -72,19 +35,4 @@ class IMapGenerator
             return true;
         };
 
-        /* float getRandPosition(float min, float max)
-        {
-            float random = min + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (max - min)));
-            return random;
-        }; */
-
-        /* Point toFindNearest(Graph g, Point p)
-        {
-            return (Point{0, 0});
-        }; */
-
-        /* bool isReachedGate(Point p)
-        {
-            return true;
-        }; */
 }
